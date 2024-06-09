1d10916523541eacc283c3ba4cb72eca0d0a192c
pilipchu <114575426+pilipchu@users.noreply.github.com>
2024-06-09 22:17:00 +0300

    add

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,7 +3,6 @@ import css from "./MovieList.module.css";
 
 export default function MovieList({ movies }) {
   const location = useLocation();
-  console.log(movies);
   return (
     <ul className={css.list}>
       {movies.map((movie) => (
